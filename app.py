6d9e04be45ccf2898189452c19db35756f7d50fc
srikanththella <90952102+srikanththella@users.noreply.github.com>
2025-01-16 15:16:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import pickle
 import numpy as np
-
-#Load the trained model
-model_path = 'final_random_forest_model.pkl'
-with open(model_path,'rb') as file:
-    model = pickle.load(file)
-
+ 
 app = Flask(__name__)
-
+ 
+# Load the pre-trained model
+model_path = 'final_random_forest_model.pkl'
+with open(model_path, 'rb') as file:
+    final_rf_model = pickle.load(file)
+ 
 @app.route('/')
 def home():
-    return render_template('app.html')
-
-@app.route('/predict',methods=['POST'])
+    return render_template('index.html')
+ 
+@app.route('/predict', methods=['POST'])
 def predict():
-    #Extract data from form
-    features = [float(x) for x in request.form.values()]
-    final_features = [np.array(features)]
-    # print(final_features)
-    #Make prediction
-    prediction = model.predict(final_features)
-    output = 'Good Air' if prediction[0]==1 else 'Poor Air'
-
-    return render_template('app.html',prediction_text = 'Prediction:{}'.format(output))
-
-
-if __name__=="__main__":
+    # Extract data from form
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features)]
+ 
+    # Make Prediction
+    prediction = final_rf_model.predict(final_features)
+ 
+    if prediction[0] == 1:
+        prediction_text = 'Good'
+    else:
+        prediction_text = 'Poor'
+ 
+    # Redirect to result.html with prediction_text
+    return render_template('result.html', prediction_text=prediction_text)
+ 
+# @app.route('/result')
+# def result():
+#     prediction_text = request.args.get('prediction')
+#     return render_template('result.html', prediction_text=prediction_text)
+ 
+if __name__ == "__main__":
     app.run(debug=True)
